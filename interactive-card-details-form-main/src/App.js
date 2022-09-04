8416fda6167f6d623e89c93894c54c8a9722f728
Stalius4 <deivydasrimgaila@gmail.com>
2022-09-04 18:42:20 +0100

    front part aded without js

--- interactive-card-details-form-main/src/App.js
+++ interactive-card-details-form-main/src/App.js
@@ -1,65 +1,82 @@
 import leftPic from "./images/bg-main-desktop.png"
+import cardLogo from "./images/card-logo.svg"
+import backCard from "./images/bg-card-back.png"
 import './App.css';
 
 function App() {
   return (
+    <div className="main-box">
 
-<div className="main-box">
-  <img className="left-bg" src={leftPic} alt="left bg" />
-0000 0000 0000 0000
-Jane Appleseed
-00/00
 
-000
 
-<div className="input-box">
-<form className="input-inner" >
-        <label className="flex-column">
-          Cardholder Name
-          <input className="name-input" type="text"  placeholder="e.g. Jane Appleseed" />
-        </label>
-        <label className="flex-column">
-        Card Number
-          <input className="name-input" type="text"  placeholder="e.g. 1234 5678 9123 0000" />
-        </label>
-       <div className="flex-row">
-        <label className="flex-column ">
-        Exp. Date (MM/YY)
-        <div className="flex-row">
-          <input className="numbers-input" type="text"  placeholder="MM" />
-          <input className="numbers-input" type="text"  placeholder="YY" />
+      <div className="front-card">
+        <img src={cardLogo} alt="card logo" className="logo-position"/>
+        <div className="card-numbers">0000 0000 0000 0000</div>
+        <div className="flex-row space-between">
+          <div>Jane Appleseed</div>
+          <div>00/00</div>
         </div>
-        </label>
-        <label className="flex-column">
-        CVC
-          <input className="numbers-input" type="text"  placeholder="e.g. 123" />
-        </label>
-        </div>
-        <input className="name-input" type="submit" value="Confirm" />
-      </form>
- 
+      </div>
 
 
-
-
-
-
-
-
-
-
-
-
-
-</div>
-{/* <!-- Completed state start --> */}
-
-Thank you!
-We've added your card details
-Continue
-
-</div>
-
+      <div className="back-card">
+     <div className="back-card-nr">000</div> 
+   </div>
+      <img className="left-bg" src={leftPic} alt="left bg" />
+    
+      <div className="input-box">
+        <form className="input-inner">
+          <label className="flex-column uppercase">
+            Cardholder Name
+            <input
+              className="name-input input-style"
+              type="text"
+              placeholder="e.g. Jane Appleseed"
+            />
+          </label>
+          <label className="flex-column uppercase ">
+            Card Number
+            <input
+              className="name-input input-style"
+              type="text"
+              placeholder="e.g. 1234 5678 9123 0000"
+            />
+          </label>
+          <div className="flex-row  gap-20">
+            <label className="flex-column uppercase">
+              Exp. Date (MM/YY)
+              <div className="flex-row gap-5">
+                <input
+                  className="numbers-input input-style"
+                  type="text"
+                  placeholder="MM"
+                />
+                <input
+                  className="numbers-input input-style"
+                  type="text"
+                  placeholder="YY"
+                />
+              </div>
+            </label>
+            <label className="flex-column uppercase">
+              CVC
+              <input
+                className="numbers-input width-50 input-style"
+                type="text"
+                placeholder="e.g. 123"
+              />
+            </label>
+          </div>
+          <input
+            className="name-input submit-btn "
+            type="submit"
+            value="Confirm"
+          />
+        </form>
+      </div>
+      {/* <!-- Completed state start --> */}
+      Thank you! We've added your card details Continue
+    </div>
   );
 }
 
