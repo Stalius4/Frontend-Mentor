f5aa3be1bcafdea1224998a16a8ba9f80cbef54a
Stalius4 <deivydasrimgaila@gmail.com>
2022-10-27 11:19:53 +0100

    desktop version added

---
+++ advice-generator/README-template.md
@@ -0,0 +1,47 @@
+# Frontend Mentor - Advice generator app solution
+
+This is a solution to the [Advice generator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/advice-generator-app-QdUG-13db). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Overview
+
+### The challenge
+
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Generate a new piece of advice by clicking the dice icon
+
+### Screenshot
+
+![](/images/Credit-card-form.png)
+
+
+
+### Links
+
+- Live Site URL: [Add live site URL here](https://bejewelled-pie-231382.netlify.app/)
+
+## My process
+
+I decided to complete all Frontend mentor challenges  with junior difficulty. This is my second challenge. Simple box with text taken from [Advice slip API](https://api.adviceslip.com/) 
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- [React](https://reactjs.org/) - JS library
+
+
+
+
+## Author
+
+
+- Frontend Mentor - [@Stalius4](https://www.frontendmentor.io/profile/yourusername)
+- Linkedin - [Deivydas Rimgaila](https://www.linkedin.com/in/deivydas-rimgaila-8aa787218/)
+
+
+
---
+++ advice-generator/src/App.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import divider from "./images/pattern-divider-desktop.svg";
+import dice from "./images/icon-dice.svg";
+function App() {
+  const [adviceId, setAdviceId] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("https://api.adviceslip.com/advice");
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      setAdviceId([data.slip]);
+      console.log(adviceId);
+    } catch (error) {}
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleChange = () => {
+    fetchData();
+  };
+
+  return (
+    <div className="main-box">
+      <div className="advice-box">
+        {adviceId.map((item, index) => {
+          return (
+            <div key={index} className="innerbox-flex">
+              <div className="advice-id">ADVICE #{item.id}</div>
+              <div className="advice-text">"{item.advice}"</div>
+            </div>
+          );
+        })}
+        <div className="images-flex">
+          <img src={divider} alt="divider" />
+          <div className="green-circle" onClick={handleChange}>
+            <img src={dice} alt="dice" className="dice" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
